de21b25ef0ab5bff99d9e59c07a4cefd0a0df0b3
root <rosivan7qi@gmail.com>
2018-10-01 23:03:30 -0300

    Lá vai uma penca de arquivos...que fez funcionar o envio por email

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form action="index.php" method="POST">
+<input type="text" name="nome">
+<input type="text" name="data">
+<input type="text" name="email">	
+
+<input type="submit" value="GERAR">
+</form>
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'swift/vendor/autoload.php';
+
+use Endroid\QrCode\QrCode;
+use Respect\Validation\Validator as v;
+$nome = $_POST['nome'];
+$data = $_POST['data'];
+$email = $_POST['email'];
+
+$validarEmail = v::email()->validate($email);
+$validarData = v::date('d-m-Y')->validate($data);
+
+if($validarEmail && $validarData){
+	$qrCode = new QrCode($nome);
+
+	header('Content-Type: '.$qrCode->getContentType());
+	echo $qrCode->writeString();
+
+
+	//Swift_Mailer
+	$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+		->setUsername('rosivan7qi@gmail.com')
+		->setPassword('.rosi123')
+	;
+
+	// Create the Mailer using your created Transport
+	$mailer = new Swift_Mailer($transport);
+
+	$msg = "Os dados do formulário são: <br>
+	Nome: $nome <br>
+	Data de Nascimento: $data <br>
+	Email: $email.<br>
+	QRCode: $qrCode><br>";
+
+	// Create a message
+	$message = (new Swift_Message('Email enviado com sucesso'))
+		->setFrom(['rosivan7qi@gmail.com' => 'Rosivan Nascimento Santos'])
+		->setTo(['rosivan7qi@gmail.com'])
+		->setBody($msg, 'text/html')
+	;
+
+	// Send the message
+	$result = $mailer->send($message);
+
+}else{
+	die('Preecha todos os campos');
+} 
+
+
+
+?>
